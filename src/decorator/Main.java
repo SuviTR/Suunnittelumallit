518e9765767d3944fed9f066025c595cf31bbb82
SuviTR <36501081+SuviTR@users.noreply.github.com>
2019-11-05 11:45:38 +0200

    Lisätty pizzojen nimet tulostukseen

--- src/decorator/Main.java
+++ src/decorator/Main.java
@@ -5,17 +5,15 @@ public class Main {
     
     public static void main(String[] args) {
         Pizza decoratedAnanasHerkkusieniPizza = new AnanasDecorator(new HerkkusieniDecorator(new SimplePizza()));
-        System.out.println(decoratedAnanasHerkkusieniPizza.getDescription());
+        System.out.println("Ananas-Herkkusieni" + decoratedAnanasHerkkusieniPizza.getDescription());
         System.out.println("Yhteensä: " + decoratedAnanasHerkkusieniPizza.getKokonaisHinta() + "€\n");
         
         Pizza decoratedAnanasAurajuustoPizza = new AnanasDecorator(new AurajuustoDecorator(new SimplePizza()));
-        System.out.println(decoratedAnanasAurajuustoPizza.getDescription());
+        System.out.println("Ananas-Aurajuusto" + decoratedAnanasAurajuustoPizza.getDescription());
         System.out.println("Yhteensä: " + decoratedAnanasAurajuustoPizza.getKokonaisHinta() + "€\n");
         
         Pizza decoratedMozzarellaHerkkusieniPizza = new MozzarellaDecorator(new HerkkusieniDecorator(new SimplePizza()));
-        System.out.println(decoratedMozzarellaHerkkusieniPizza.getDescription());
+        System.out.println("Mozzarella-Herkkusieni" + decoratedMozzarellaHerkkusieniPizza.getDescription());
         System.out.println("Yhteensä: " + decoratedMozzarellaHerkkusieniPizza.getKokonaisHinta() + "€\n");
-        
-        
     }
 }
